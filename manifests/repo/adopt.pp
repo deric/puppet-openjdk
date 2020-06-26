2d8efc633a7a59bf553b27a1c661125efa9f1ebf
Tomas Barton <barton.tomas@gmail.com>
2020-06-26 18:48:38 +0200

    Debian repo, add sync modifications

---
+++ manifests/repo/adopt.pp
@@ -0,0 +1,33 @@
+# @summary Manage AdoptOpenJDK repo
+#
+# @api private
+class openjdk::repo::adopt {
+
+  case $facts['os']['family'] {
+    'RedHat', 'Linux': {
+      # TODO
+    }
+
+    'Debian': {
+      include ::apt
+
+      apt::source { 'adoptopenjdk':
+        location => 'https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/',
+        release  => $::lsbdistcodename,
+        repos    => 'main',
+        key      => {
+          id     => '8ED17AF5D7E675EB3EE3BCE98AC3B29174885C03',
+          source => 'https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public',
+        },
+        include  => {
+          src => false,
+        },
+      }
+    }
+
+    default: {
+      fail("Unsupported managed repository for osfamily: ${::osfamily}, operatingsystem: ${::operatingsystem}, module ${module_name} currently only supports managing repos for osfamily RedHat and Debian")
+    }
+  }
+
+}
